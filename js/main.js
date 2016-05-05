1de5c121a8f1f6e796fe1106a2aecea170db95c4
Piet Brauer <piet@nerdishbynature.com>
2016-05-05 09:37:27 +0700

    Don’t ignore whitespace

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var Diff = require('diff');
 
 swift_diffStrings = function(oldLine, newLine) {
-	return Diff.diffWords(oldLine, newLine);
+    return Diff.diffWords(oldLine, newLine, {"ignoreWhitespace": false});
 }
